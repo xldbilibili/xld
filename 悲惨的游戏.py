17ce34e923c45daf9b1b0b3ac5adf8965b370940
石昊谦 <92210707+xldbilibili@users.noreply.github.com>
2022-09-13 13:06:09 +0800

    Add files via upload

---
+++ 悲惨的游戏.py
@@ -0,0 +1,15 @@
+"""""" # 声明
+print("this game was made by XLD")
+
+print("we are going to start")
+""""""
+# for PC
+user_name = input("你名叫啥？！！")
+user_age = int(input("你多大了"))
+#for user
+print('nihao' , user_name)
+if user_age>=18:
+    print("welcome")
+else:
+    print("滚 未成年")
+
