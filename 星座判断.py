c78facd85634726e5eab189cf80338b00afd980f
石昊谦 <92210707+xldbilibili@users.noreply.github.com>
2023-04-02 08:33:36 +0800

    Add files via upload

--- 星座判断.py
+++ 星座判断.py
@@ -4,30 +4,30 @@ month = int(input("请输入出生月份"))
 day = int(input("请输入出生日期"))
 birthday = 100*month + day
 
-if  (int(birthday) >= 120 and int(birthday) <= 218) :
+if 120 <= int(birthday) <= 218:
     print("水瓶座")
-elif (int(birthday) >= 219 and int(birthday) <= 320) :
+elif 219 <= int(birthday) <= 320:
     print("双鱼座")
-elif (int(birthday) >= 321 and int(birthday) <= 419) :
+elif 321 <= int(birthday) <= 419:
     print("白羊座")
-elif (int(birthday) >= 420 and int(birthday) <= 520) :
+elif 420 <= int(birthday) <= 520:
     print("金牛座")
-elif (int(birthday) >= 521 and int(birthday) <= 621) :
+elif 521 <= int(birthday) <= 621:
     print("双子座")
-elif (int(birthday) >= 622 and int(birthday) <= 722) :
+elif 622 <= int(birthday) <= 722:
     print("巨蟹座")
-elif (int(birthday) >= 723 and int(birthday) <= 822) :
+elif 723 <= int(birthday) <= 822:
     print("狮子座")
-elif (int(birthday) >= 823 and int(birthday) <= 922) :
+elif 823 <= int(birthday) <= 922:
     print("处女座")
-elif (int(birthday) >= 923 and int(birthday) <= 1023) :
+elif 923 <= int(birthday) <= 1023:
     print("天秤座")
-elif (int(birthday) >= 1024 and int(birthday) <= 1122) :
+elif 1024 <= int(birthday) <= 1122:
     print("天蝎座")
-elif(int(birthday) >= 1123 and int(birthday) <= 1221) :
+elif 1123 <= int(birthday) <= 1221:
     print("射手座")
-elif((int(birthday) >= 1222 and int(birthday) <= 1230 )
-        or (int(birthday) >= 0 and int(birthday) <= 119)):
+elif((1222 <= int(birthday) <= 1230)
+        or (0 <= int(birthday) <= 119)):
     print("摩羯座")
-else :
+else:
     print("你找bug是吧")
